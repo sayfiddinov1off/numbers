0d02add3a26d1ec552b1d121d841fd35e5d2962b
Azamat <105981809+sayfiddinov1off@users.noreply.github.com>
2022-12-09 12:48:11 +0300

    Add files via upload

---
+++ numbers/main.js
@@ -0,0 +1,31 @@
+// 1
+// let num1 = prompt("birinchi sonni kiriting");
+// let num2 = prompt("Ikkinchi sonni kiriting");
+// function pow(x){
+//     console.log(num1 ** num2);
+// }
+// pow()
+
+// 2
+// let a = 5.75555;
+// function floor(){
+//     console.log(Math.trunc(a));
+// }
+// floor()
+
+
+// 2
+// let flov = floor(2.7324); 
+// function floor(number){ 
+//     return ~number * -1 - 1 
+// } 
+// console.log(flov);
+
+
+// 4
+// let roun = round(968.5);
+// function round(num){
+
+//     return num.toFixed(0);
+// }
+// console.log(roun);
